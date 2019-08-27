92947f7fc268ed224e2c120300250d3357bc970a
Midstackdev <midstackdev@gmail.com>
2019-08-27 20:08:42 +0100

    Vuetify, Vue-router, Initial Componenets

---
+++ resources/js/Router/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { 
+  	path: '/login', 
+  	component: Login 
+  },
+  { 
+  	path: '/', 
+  	component: Home 
+  }
+]
+
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+export default router
